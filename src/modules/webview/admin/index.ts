6fd8052825a8bd1510e8fe474c7f9c94ba52a283
Salaron <breykin.alexey@gmail.com>
2019-10-20 23:39:03 +0500

    Fix wv login actions Also add media queries in css for adoption

--- src/modules/webview/admin/index.ts
+++ src/modules/webview/admin/index.ts
@@ -48,7 +48,7 @@ export default class extends WebViewAction {
         this.requestData.auth_token = token
         return {
           status: 200,
-          result: template(values)
+          result: await webview.compileBodyTemplate(template, this.requestData, values)
         }
       }
       default: {
--- src/modules/webview/login/login.ts
+++ src/modules/webview/login/login.ts
@@ -15,24 +15,23 @@ export default class extends WebViewAction {
     if (this.requestData.auth_level != this.requiredAuthLevel && !Config.server.debug_mode)
       throw new ErrorCode(1234, "Access only with a certain auth level")
     const i18n = new I18n(this.connection)
+    const webview = new WebView(this.connection)
 
     const [strings, template] = await Promise.all([
       i18n.getStrings(<string>this.requestData.auth_token, "login-startup", "login-login"),
-      WebView.getTemplate("login", "login")
+      webview.getTemplate("login", "login")
     ])
 
     const values = {
-      headers: JSON.stringify(this.requestData.getWebapiHeaders()),
-      publicKey: JSON.stringify(Config.server.PUBLIC_KEY),
       module: "login",
-      external: this.requestData.requestFromBrowser,
       enableRecaptcha: Config.modules.login.enable_recaptcha,
       siteKey: Config.modules.login.recaptcha_site_key,
-      i18n: strings
+      i18n: strings,
+      pageTitle: "SunLight Login"
     }
     return {
       status: 200,
-      result: template(values)
+      result: await webview.compileBodyTemplate(template, this.requestData, values)
     }
   }
 }
