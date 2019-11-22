061f87caae2b72fdfdff2f8b60c54ff1fb258665
Salaron <breykin.alexey@gmail.com>
2019-11-22 21:48:34 +0500

    6.9 banners

---
+++ src/db/migrate/03_support_new_banners.ts
@@ -0,0 +1,13 @@
+import { Connection } from "../../core/database/mariadb"
+import { Log } from "../../core/log"
+
+const log = new Log("rLive")
+
+export async function migrate(connection: Connection) {
+  log.info("In 6.9 was added banners with id")
+  const banners = (await connection.query("SELECT * FROM banner_list")).map(banner => {
+    return `INSERT INTO banner_list (banner_type, target_id, asset_path, asset_path_se, member_category, webview_url, master_is_active_event, start_date, end_date) VALUES (${banner.banner_type}, ${banner.target_id}, ${banner.asset_path}, ${banner.asset_path_se}, ${banner.member_category}, ${banner.webview_url}, ${banner.master_is_active_event}, ${banner.start_date}, ${banner.end_date})`
+  })
+  console.log(banners.join(";\n"))
+  await connection.query(`DROP TABLE banner_list;`)
+}
--- src/modules/api/banner/bannerlist.ts
+++ src/modules/api/banner/bannerlist.ts
@@ -3,6 +3,8 @@ import { REQUEST_TYPE, PERMISSION, AUTH_LEVEL } from "../../../models/constant"
 import moment from "moment"
 import { Utils } from "../../../common/utils"
 
+const bannerDB = sqlite3.getBanner()
+
 export default class extends ApiAction {
   public requestType: REQUEST_TYPE = REQUEST_TYPE.BOTH
   public permission: PERMISSION = PERMISSION.NOXMC
@@ -13,70 +15,36 @@ export default class extends ApiAction {
   }
 
   public async execute() {
-    const museBanners = await this.connection.query(`
-    SELECT * FROM (
-      SELECT banner_asset_name as asset_path, banner_se_asset_name as asset_path_se, secretbox_id as target_id, 1 as banner_type, 0 as master_is_active_event, member_category, NULL as webview_url, start_date, end_date FROM secretbox_list
-      UNION
-      SELECT banner_asset_name as asset_path, banner_se_asset_name as asset_path_se, event_id as target_id, 0 as banner_type, 1 as master_is_active_event, member_category, NULL as webview_url, open_date as start_date, close_date as end_date FROM events_list
-    	UNION
-    	SELECT  asset_path, asset_path_se, target_id, banner_type, master_is_active_event, member_category, webview_url, start_date, end_date FROM banner_list
-    ) as b WHERE asset_path IS NOT NULL AND asset_path_se IS NOT NULL AND start_date < :now AND end_date > :now ORDER BY member_category ASC, master_is_active_event DESC`, {
-      now: Utils.toSpecificTimezone(9)
-    })
-    const aqoursBanners = await this.connection.query(`
-    SELECT * FROM (
-      SELECT banner_asset_name as asset_path, banner_se_asset_name as asset_path_se, secretbox_id as target_id, 1 as banner_type, 0 as master_is_active_event, member_category, NULL as webview_url, start_date, end_date FROM secretbox_list
-      UNION
-      SELECT banner_asset_name as asset_path, banner_se_asset_name as asset_path_se, event_id as target_id, 0 as banner_type, 1 as master_is_active_event, member_category, NULL as webview_url, open_date as start_date, close_date as end_date FROM events_list
-    	UNION
-    	SELECT asset_path, asset_path_se, target_id, banner_type, master_is_active_event, member_category, webview_url, start_date, end_date FROM banner_list
-    ) as b WHERE asset_path IS NOT NULL AND asset_path_se IS NOT NULL AND start_date < :now AND end_date > :now ORDER BY member_category DESC, master_is_active_event DESC`, {
-      now: Utils.toSpecificTimezone(9)
-    })
-
-    const museCategory = {
-      member_category: 1,
-      banner_list: museBanners.map(banner => {
-        return {
-          banner_type: banner.banner_type,
-          target_id: banner.target_id,
-          asset_path: banner.asset_path,
-          asset_path_se: banner.asset_path_se,
-          fixed_flag: false,
-          back_side: false,
-          master_is_active_event: banner.master_is_active_event === 1 ? true : undefined,
-          banner_id: 1,
-          start_date: banner.start_date,
-          end_date: banner.end_date || "2037-12-31 23:59:59"
-        }
-      })
-    }
-    const aqoursCategory = {
-      member_category: 2,
-      banner_list: aqoursBanners.map(banner => {
-        return {
-          banner_type: banner.banner_type,
-          target_id: banner.target_id,
-          asset_path: banner.asset_path,
-          asset_path_se: banner.asset_path_se,
-          fixed_flag: false,
-          back_side: false,
-          master_is_active_event: banner.master_is_active_event === 1 ? true : undefined,
-          banner_id: 1,
-          start_date: banner.start_date,
-          end_date: banner.end_date || "2037-12-31 23:59:59"
-        }
-      })
-    }
-
-    const response = {
-      time_limit: moment(new Date()).utcOffset("+0900").add(1, "hour").format("YYYY-MM-DD HH:mm:ss"),
-      member_category_list: [museCategory, aqoursCategory]
-    }
-
     return {
       status: 200,
-      result: response
+      result: {
+        time_limit: moment(new Date()).utcOffset("+0900").add(1, "h").format("YYYY-MM-DD HH:mm:ss"),
+        member_category_list: [await this.getBannerList(1), await this.getBannerList(2)]
+      }
+    }
+  }
+
+  private async getBannerList(memberCategory: memberCategory) {
+    const list = (await bannerDB.all(`SELECT * FROM banner WHERE date(start_date) <= date(:now) AND date(end_date) > date(:now) ORDER BY member_category ${memberCategory === 1 ? "ASC" : "DESC"}`, {
+      now: Utils.toSpecificTimezone(9)
+    })).map(banner => {
+      return {
+        banner_type: banner.type,
+        target_id: banner.target,
+        asset_path: banner.asset_path,
+        asset_path_se: banner.asset_path_se,
+        fixed_flag: banner.fixed_flag ? true : false,
+        back_side: banner.back_side ? true : false,
+        banner_id: banner.id,
+        start_date: banner.start_date,
+        end_date: banner.end_date,
+        webview_url: banner.webview_url
+      }
+    })
+
+    return {
+      member_category: memberCategory,
+      banner_list: list
     }
   }
 }
