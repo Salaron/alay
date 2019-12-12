118fb6b78f10d4250995863ebeaf28a634d4d674
Salaron <breykin.alexey@gmail.com>
2019-12-12 15:23:05 +0500

    Update banner migration

--- src/db/migrate/03_support_new_banners.ts
+++ src/db/migrate/03_support_new_banners.ts
@@ -6,7 +6,7 @@ const log = new Logger("rLive")
 export async function migrate(connection: Connection) {
   log.info("In 6.9 was added banners with id")
   const banners = (await connection.query("SELECT * FROM banner_list")).map(banner => {
-    return `INSERT INTO banner_list (banner_type, target_id, asset_path, asset_path_se, member_category, webview_url, master_is_active_event, start_date, end_date) VALUES (${banner.banner_type}, ${banner.target_id}, ${banner.asset_path}, ${banner.asset_path_se}, ${banner.member_category}, ${banner.webview_url}, ${banner.master_is_active_event}, ${banner.start_date}, ${banner.end_date})`
+    return `INSERT INTO banner_list (type, target, asset_path, asset_path_se, member_category, webview_url, start_date, end_date) VALUES (${banner.banner_type}, ${banner.target_id}, ${banner.asset_path}, ${banner.asset_path_se}, ${banner.member_category}, ${banner.webview_url}, ${banner.start_date}, ${banner.end_date})`
   })
   console.log(banners.join(";\n"))
   await connection.query(`DROP TABLE banner_list;`)
