9eddf3d8d34196bec824345859504543b12ce80c
Salaron <breykin.alexey@gmail.com>
2020-04-15 01:11:18 +0500

    Update partner icon

--- src/common/webview.ts
+++ src/common/webview.ts
@@ -5,6 +5,19 @@ import { BaseAction } from "../models/actions"
 import { CommonModule } from "../models/common"
 import { Utils } from "./utils"
 
+let unitIcons: { [unitId: number]: { 1: string, 2: string }} = {}
+
+const unitDB = sqlite3.getUnitDB()
+export async function init() {
+  const allUnits = await unitDB.all("SELECT unit_id, unit_number, normal_icon_asset, rank_max_icon_asset FROM unit_m")
+  allUnits.map(unit => {
+    unitIcons[unit.unit_id] = {
+      1: `icon/${unit.unit_number}.png`,
+      2: `iconRankup/${unit.unit_number}.png`
+    }
+  })
+}
+
 let assets = JSON.parse(readFileSync("webpack-assets.json", "utf-8"))
 export class WebView extends CommonModule {
   constructor(action: BaseAction) {
@@ -20,6 +33,13 @@ export class WebView extends CommonModule {
     if (Config.server.debug_mode) {
       assets = JSON.parse(await promises.readFile("webpack-assets.json", "utf-8"))
     }
+    let partnerUnitIcon = null
+    if (this.requestData.user_id) {
+      const partner: { unit_id: number, rank: 1 | 2 } = await this.connection.first("SELECT unit_id, \`rank\` FROM users JOIN units ON users.partner_unit = units.unit_owning_user_id WHERE users.user_id = :userId", {
+        userId: this.userId
+      })
+      partnerUnitIcon = unitIcons[partner.unit_id][partner.rank]
+    }
     const defaultLocals: Pug.LocalsObject = {
       pageTitle: "SunLight WebView", // page title can be overridden
       ...locals,
@@ -31,7 +51,8 @@ export class WebView extends CommonModule {
       isAdmin: Config.server.admin_ids.includes(this.requestData.user_id || 0),
       languageList: Config.i18n.languages,
       webpackAssets: assets,
-      currentLanguage
+      currentLanguage,
+      partnerUnitIcon
     }
 
     return Pug.renderFile(`./webview/view/${module}/${action}.pug`, {
