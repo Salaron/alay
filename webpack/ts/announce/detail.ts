fe528b48f1fccaa0ba9842e32be0a872175dcb4c
Salaron <breykin.alexey@gmail.com>
2020-04-18 20:01:21 +0500

    Webpack: change target to ES5 for IE support

--- webpack/ts/announce/detail.ts
+++ webpack/ts/announce/detail.ts
@@ -9,7 +9,7 @@ $(() => {
   if (!isWebview) {
     $('a[href^="http"]').each(function () {
       if (typeof $(this).attr("href") !== "string") return
-      if ($(this).attr("href")?.includes("webview.php/announce/index")) return
+      if ($(this).attr("href")!.includes("webview.php/announce/index")) return
       $(this).attr("href", getExternalURL($(this).attr("href")))
     })
   }
