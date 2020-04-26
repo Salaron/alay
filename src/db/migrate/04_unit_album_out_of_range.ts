0f78bc4e570b219b7a88da0fa4740146c88af478
Salaron <breykin.alexey@gmail.com>
2020-04-26 11:17:16 +0500

    Remove migration script We need to find some other way to do migrations

--- src/db/migrate/04_unit_album_out_of_range.ts
+++
@@ -1,9 +0,0 @@
-import { Connection } from "../../core/database/mysql"
-import { Logger } from "../../core/logger"
-
-const log = new Logger("rLive")
-
-export async function migrate(connection: Connection) {
-  log.info("There was a type error in user_unit_album...")
-  await connection.query(`ALTER TABLE user_unit_album	CHANGE COLUMN highest_love_per_unit highest_love_per_unit INT UNSIGNED NOT NULL DEFAULT 0 AFTER all_max_flag`)
-}
