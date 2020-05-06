8af4ca97bc0fe4f8e18c26146a126c701f1e3481
Salaron <breykin.alexey@gmail.com>
2020-05-06 23:50:47 +0300

    placeholder for unfinished pages

---
+++ src/modules/webview/profile/index.ts
@@ -0,0 +1,22 @@
+import RequestData from "../../../core/requestData"
+import { AUTH_LEVEL, WV_REQUEST_TYPE } from "../../../models/constant"
+
+export default class extends WebViewAction {
+  public requestType: WV_REQUEST_TYPE = WV_REQUEST_TYPE.BOTH
+  public requiredAuthLevel: AUTH_LEVEL = AUTH_LEVEL.CONFIRMED_USER
+
+  constructor(requestData: RequestData) {
+    super(requestData)
+  }
+
+  public async execute() {
+    // TODO
+    return {
+      status: 200,
+      result: await this.webview.renderTemplate("common", "notready", {
+        pageTitle: "Profile",
+        i18n: await this.i18n.getStrings()
+      })
+    }
+  }
+}
